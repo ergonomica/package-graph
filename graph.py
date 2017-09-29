ece8741b58f0d10244c9655bbfcee9ce7bbf315e
Liam Schumm <liamschumm@icloud.com>
2017-09-28 22:50:52 -0500

    Update grapher package to latest command syntax; allow for multiple graphs

--- graph.py
+++ graph.py
@@ -1,3 +1,5 @@
+# -*- encoding: utf-8 -*-
+
 #
 # [graph.py]
 #
@@ -8,28 +10,35 @@
 
 ticks = ["▁", "▂", "▃", "▄", "▅", "▆ ", "▇ ", "█"]
 
-def main(argc):
+def graph(*numbers_s):
     """graph: Graph numbers in your terminal.
     
     Usage:
         graph NUMBERS...
     """
     
-    args = [int(x) for x in argc.args['NUMBERS']]
+    out = []
     
-    low = min(args)
-    high = max(args)
-    step = (high - low) / 8.0
+    for numbers in numbers_s:
+        args = [int(x) for x in numbers]
+    
+        low = min(args)
+        high = max(args)
+        step = (high - low) / 8.0
 
-    # if graph is constant, use highest character
-    if low == high:
-        return ticks[-1] * len(args)
+        # if graph is constant, use highest character
+        if low == high:
+            return ticks[-1] * len(args)
     
-    else:
-        # string where completed graph goes
-        s = ""
-        for arg in args:
-            for i in range(8):
-                if low + i * step <= arg <= low + (i+1) * step:
-                    s += ticks[i]
-        return s
+        else:
+            # string where completed graph goes
+            s = ""
+            for arg in args:
+                for i in range(8):
+                    if low + i * step <= arg <= low + (i+1) * step:
+                        s += ticks[i]
+            out.append(s)
+    
+    return out
+
+exports = {'graph': graph}
